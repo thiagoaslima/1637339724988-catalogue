e86d5c28219a20d0c002ed3f9053c1750d84ab6f
Thiago Lima <thiagoaslima@gmail.com>
2021-11-20 22:58:21 -0700

    test: Add NotFound route tests

--- src/ui/__tests__/routes.spec.jsx
+++ src/ui/__tests__/routes.spec.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {MemoryRouter} from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import {mount, configure} from 'enzyme';
+
 import App from '../components/App';
 import Home from '../components/Home';
 import NotFound from '../components/NotFound';
@@ -8,11 +9,21 @@ import NotFound from '../components/NotFound';
 describe('Application routes', () => {
   it('should render a Home component', () => {
     const wrapper = mount(
-      <MemoryRouter initialEntries={['/']}>
-        <App />
-      </MemoryRouter>
+      <App history={createMemoryHistory({
+        initialEntries: ['/']
+      })} />  
     );
 
     expect(wrapper.find(Home)).toHaveLength(1);
+  });
+  
+  it('unknown routes (404) should render a NotFound component', () => {
+    const wrapper = mount(
+      <App history={createMemoryHistory({
+        initialEntries: ['/unknown']
+      })} />
+    );
+
+    expect(wrapper.find(NotFound)).toHaveLength(1);
   });
 });
